7eb95e2c04ac0c9436fd153ac0ae0e9077453710
Rima Paulionė <rimapaulione@gmail.com>
2026-02-24 14:09:24 +0200

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,44 @@
+import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  //With  signals input and output
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  //id=input.required<string>();
+
+  //select = output<string>();
+
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter<string>();
+
+  //With signals
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
